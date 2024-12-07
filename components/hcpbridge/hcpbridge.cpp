3b141305bba3731a127fb8a12f8ad0e971a8379b
14yannick <14yannick@gmail.com>
2024-12-07 23:03:15 +0100

    Optimize Blocking time
    Create a second Callback manager to reduce the blocking time during a single loop.
    Set conver assumed state to false and publish it during the on_event_triggered function.

--- components/hcpbridge/hcpbridge.cpp
+++ components/hcpbridge/hcpbridge.cpp
@@ -1,28 +1,53 @@
 #include "hcpbridge.h"
 
-namespace esphome
-{
-  namespace hcpbridge
-  {
+namespace esphome {
+namespace hcpbridge {
 
-    void HCPBridge::setup()
-    {
-      int8_t rx = this->rx_pin_ == nullptr ? PIN_RXD : this->rx_pin_->get_pin();
-      int8_t tx = this->tx_pin_ == nullptr ? PIN_TXD : this->tx_pin_->get_pin();
-      int8_t rts = this->rts_pin_ == nullptr ? -1 : this->rts_pin_->get_pin();
+static const char *TAG = "hcpbridge";
+void HCPBridge::setup() {
+  int8_t rx = this->rx_pin_ == nullptr ? PIN_RXD : this->rx_pin_->get_pin();
+  int8_t tx = this->tx_pin_ == nullptr ? PIN_TXD : this->tx_pin_->get_pin();
+  int8_t rts = this->rts_pin_ == nullptr ? -1 : this->rts_pin_->get_pin();
 
-      this->engine = &HoermannGarageEngine::getInstance();
-      this->engine->setup(rx, tx, rts);
+  this->engine = &HoermannGarageEngine::getInstance();
+  this->engine->setup(rx, tx, rts);
+}
+void HCPBridge::add_on_state_callback(std::function<void()> &&callback) {
+  auto wrapped_callback = [callback]() {
+    auto start = millis();
+    callback();
+    auto end = millis();
+    ESP_LOGD(TAG, "Callback executed in %u ms", end - start);
+  };
+  this->state_callback_.add(std::move(wrapped_callback));
+}
+
+void HCPBridge::add_prio_callback(std::function<void()> &&callback, const char *tag) {
+  auto wrapped_callback = [callback, tag]() {
+    auto start = millis();
+    callback();
+    auto end = millis();
+    ESP_LOGD(TAG, "Callback executed in %u ms [Tag: %s]", end - start, tag);
+  };
+  this->prio_callback_.add(std::move(wrapped_callback));
+}
+
+void HCPBridge::update() {
+  static bool is_prio_callback_next = true;
+
+  if (this->engine->state->changed) {
+    if (is_prio_callback_next) {
+      this->prio_callback_.call();
+    } else {
+      this->state_callback_.call(); 
     }
-    void HCPBridge::add_on_state_callback(std::function<void()> &&callback){
-      this->state_callback_.add(std::move(callback)); 
-    }
 
-    void HCPBridge::update(){
-      if (this->engine->state->changed) {
-        this->engine->state->clearChanged();
-        this->state_callback_.call();
-      }
+    is_prio_callback_next = !is_prio_callback_next;
+
+    if (is_prio_callback_next) { 
+      this->engine->state->clearChanged();
     }
   }
 }
+}  // namespace hcpbridge
+}  // namespace esphome
